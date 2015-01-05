8c93e6e2c885221c2f3220238191dfe505e6a255
moinulmithu <moinulmithu@hotmail.com>
2015-01-05 22:44:16 +0600

    Chatting Application

---
+++ ChatApp/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ChatApp
+{
+    public class ChatHub : Hub
+    {
+        public void send(string username, string message)
+        {
+            Clients.All.broadcastMessage(username, message);
+        }
+    }
+}
---
+++ ChatApp/app/scripts/controllers/chatController.js
@@ -0,0 +1,18 @@
+﻿app.controller('chatController', ['$scope', function ($scope) {
+
+    $scope.messages = [];
+    var chat = $.connection.chatHub;
+
+    chat.client.broadcastMessage = function (name, message) {
+        $scope.messages.push({ UserName: name, Message: message });
+        $scope.$apply();
+    };
+    $.connection.hub.start().done(function() {
+        
+    });
+
+    //angular
+    $scope.send = function() {
+        chat.server.send($scope.username, $scope.message);
+    };
+}]);
